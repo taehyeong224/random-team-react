e7d0446741b601736731084a47b6060d090874f8
김태형 <taehyeong224@gmail.com>
2019-10-23 17:26:57 +0900

    add again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Provider} from 'react-redux'
 import './css/App.css';
 import {MyHeader} from "./components/MyHeader";
-import {MyButton} from "./components/MyButton";
 import {MyLabel} from "./components/MyLabel";
 import storeFactory from './store'
 import {ClearName, NewName, ShakeName, ShowNameList, ShowShuffleList, TeamSetting} from "./components/containers";
@@ -26,7 +25,7 @@ function App() {
                 <TeamSetting/>
                 <ClearName {...ButtonList[0]}/>
                 <ShakeName {...ButtonList[1]}/>
-                <MyButton  {...ButtonList[2]}/>
+                <ShakeName {...ButtonList[2]}/>
                 <MyLabel className={"result"} text={"result"}/>
                 <ShowShuffleList/>
             </div>
--- src/components/ShuffleList.js
+++ src/components/ShuffleList.js
@@ -2,18 +2,24 @@ import React from "react"
 import "../css/ShuffleList.css"
 
 export const ShuffleList = ({shuffleList = []}) => {
-    const resultStyle = {
-        borderRadius: "30px",
-        textAlign: "center",
-        lineHeight: "20px",
-        color: "rgb(255, 255, 255)",
-        fontSize: "12px",
-        boxShadow: "none"
+    const getRandomStyle = () => {
+        return {
+            borderRadius: "30px",
+            textAlign: "center",
+            lineHeight: "20px",
+            color: "rgb(255, 255, 255)",
+            fontSize: "12px",
+            boxShadow: "none",
+            backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+        }
     };
+    const rand = React.useMemo(() => Math.random(), []) ;
+
     return <div className={"shuffle-list"}>
+        {rand}
         {shuffleList.map((team, i) => {
                 return <div key={i}>
-                    <div style={{...resultStyle, ...{backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`}}} key={i}>team {i + 1}</div>
+                    <div style={getRandomStyle()} key={i}>team {i + 1}</div>
                     {team.map((name, j) => <li key={j}>{name}</li>)}
                 </div>
             }
