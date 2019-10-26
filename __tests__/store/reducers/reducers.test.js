f9443f8c13c551d6259c7fc12c629bdb0e3ceaa9
김태형 <taehyeong224@gmail.com>
2019-10-27 06:18:03 +0900

    edit test

--- __tests__/store/reducers/reducers.test.js
+++ __tests__/store/reducers/reducers.test.js
@@ -3,6 +3,8 @@ import {expect} from "chai"
 import {names} from "../../../src/store/reducers";
 import v4 from "uuid";
 import {clearName, removeName} from "../../../src/actions";
+import deepFreeze from "deep-freeze";
+
 
 describe("리듀서 테스트", () => {
     describe("names", () => {
@@ -13,13 +15,16 @@ describe("리듀서 테스트", () => {
                 name: v4(),
                 id: v4()
             };
+            deepFreeze(state)
+            deepFreeze(action)
             const result = names(state, action);
             expect(result.length).to.be.eq(1);
         });
 
         it(`${REMOVE_NAME} 성공`, () => {
             const state = [];
-            const result = names(names(names(state, addNameAction()), addNameAction()), addNameAction());
+            deepFreeze(state)
+            const result = names(names(names(state, deepFreeze(addNameAction())), deepFreeze(addNameAction())), deepFreeze(addNameAction()));
             expect(names(result, removeName(result[0].id)).length).to.be.eq(2)
         });
 
